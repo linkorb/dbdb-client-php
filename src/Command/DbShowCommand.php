9cc45a3c0926980f9abcb56b7e76674c2d89d79e
kaushik.prajapati <kaushik@indianic.com>
2017-12-22 15:10:54 +0530

    Card #540: DbDb client: Db:show

--- src/Command/DbShowCommand.php
+++ src/Command/DbShowCommand.php
@@ -7,35 +7,23 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class DbPropertyCommand extends Command
+class DbShowCommand extends Command
 {
     protected function configure()
     {
         $this
-            ->setName('db:property')
-            ->setDescription('update database property')
+            ->setName('db:show')
+            ->setDescription('show database')
             ->addArgument(
                 'dbname',
                 InputArgument::REQUIRED,
                 'Name of database'
-            )
-            ->addArgument(
-                'key',
-                InputArgument::REQUIRED,
-                'Name of database property'
-            )
-            ->addArgument(
-                'value',
-                InputArgument::REQUIRED,
-                'Name of database property value'
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dbname = $input->getArgument('dbname');
-        $key = $input->getArgument('key');
-        $value = $input->getArgument('value');
 
         $url = getenv('DBDB_URL');
         $username = getenv('DBDB_USERNAME');
@@ -46,7 +34,7 @@ class DbPropertyCommand extends Command
         }
 
         $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', $url.'/api/v1/dbs/'.$dbname.'/property/'.$key.'/'.$value, [
+        $res = $client->request('GET', $url.'/api/v1/dbs/'.$dbname, [
              'auth' => [$username, $password],
         ]);
 
